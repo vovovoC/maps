76ee35632b5c6ef6158305ef84936019727f9b7b
vovovoC <vovova.vovova@list.ru>
2021-10-12 14:38:44 +0600

    company name add

--- src/Company.ts
+++ src/Company.ts
@@ -17,6 +17,6 @@ export class Company implements Mappable{
         }
     };
     markerContent():string{
-        return `Company name: <strong>${this.companyName}</strong>`;
+        return `Company_name: <strong>${this.companyName}</strong>`;
     }
 }
